e749ca7584fcba60f0c28dfa759eaf8cf08df27e
HeavyDutySoul <putsli@gmail.com>
2021-07-26 16:37:31 +0300

    Change debug pin configurations.

--- keyboards/orthrus_ble/slave/rules.mk
+++ keyboards/orthrus_ble/slave/rules.mk
@@ -1,11 +1,9 @@
 
-# it should exist either in <this_dir>/ld/
 MCU_LDSCRIPT = nrf51822_slave
 MCU_SERIES = NRF51822
 NRFSDK_ROOT := ./NRF5_SDK
 
-# project specific files
-#SRC =	matrix.c
 NRF_SEPARATE = slave
 
-#NRFCFLAGS += -DNRF_LOG_BACKEND_SERIAL_UART_TX_PIN=21
+#NRFCFLAGS += -DNRF_LOG_BACKEND_SERIAL_USES_UART=1
+#NRFCFLAGS += -DNRF_LOG_BACKEND_SERIAL_UART_TX_PIN=19
