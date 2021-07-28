e98545dfc3dbafc4d89385242da92944367403c0
HeavyDutySoul <putsli@gmail.com>
2021-07-28 18:41:16 +0300

    Add Orthrus-BLE keyboard

---
+++ keyboards/orthrus_ble/master/config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "custom_board.h"
+
+#define THIS_DEVICE_ROWS 1
+#define THIS_DEVICE_COLS 28
+
+#define ENABLE_STARTUP_ADV_NOLIST
+
+#define MATRIX_ROW_PINS { NO_PIN }
+#define MATRIX_COL_PINS { 17, 13, 9,  4, 0, 29, \
+		          18, 14, 10, 5, 1, 28, \
+		          19, 15, 11, 6, 2, 25, \
+		          20, 16, 12, 7, 3, 24, \
+				      8,30, 23, 22 \
+			}
+
+#define IS_LEFT_HAND  true
---
+++ keyboards/orthrus_ble/orthrus_ble.c
@@ -0,0 +1,1 @@
+#include "orthrus_ble.h"
