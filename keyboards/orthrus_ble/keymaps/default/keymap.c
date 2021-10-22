838c9bbc8c73d13285bee62a0a7856b819018c8b
HeavyDutySoul <putsli@gmail.com>
2021-10-22 17:08:51 +0300

    Change keyboard keymap.

--- keyboards/orthrus_ble/keymaps/default/keymap.c
+++ keyboards/orthrus_ble/keymaps/default/keymap.c
@@ -82,9 +82,9 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( 
     KC_ESC,   KC_1,    KC_2,     KC_3,     KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, \
     KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC, \
-    KC_LCTL,  KC_A,    KC_S,     KC_D,     KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, \
+    KC_LGUI,  KC_A,    KC_S,     KC_D,     KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,    KC_X,     KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT, \
-                       ADJUST,   KC_LGUI,  RAISE, KC_SPC,     KC_ENT,  LOWER,   KC_RALT, KC_BSPC \
+                       ADJUST,   KC_LCTL,  RAISE, KC_SPC,     KC_ENT,  LOWER,   KC_RALT, KC_BSPC \
   ),
 
   [_LOWER] = LAYOUT(
@@ -96,7 +96,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_RAISE] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,     XXXXXXX, KC_7,    KC_8,    KC_9,     _______,  KC_EQL,  \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_7,    KC_8,    KC_9,     XXXXXXX,  KC_EQL,  \
     _______, _______, _______, _______, _______, _______,     KC_PSCR, KC_4,    KC_5,    KC_6,     _______,  KC_RBRC, \
     _______, _______, _______, _______, _______, _______,     KC_INS,  KC_1,    KC_2,    KC_3,     _______,  _______, \
     KC_LSFT, _______, _______, _______, _______, _______,     KC_DEL,  KC_0,    KC_DOT,  KC_LT,    KC_GT,    _______, \
