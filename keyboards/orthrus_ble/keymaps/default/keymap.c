720b1e34f930b0a0471023e5567b2f8ca346a9c0
HeavyDutySoul <putsli@gmail.com>
2021-11-05 16:04:52 +0300

    Change keyboard keymap.

--- keyboards/orthrus_ble/keymaps/default/keymap.c
+++ keyboards/orthrus_ble/keymaps/default/keymap.c
@@ -19,12 +19,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "app_ble_func.h"
 #include <stdio.h>
 
-
 enum custom_keycodes {
     AD_WO_L = SAFE_RANGE, /* Start advertising without whitelist  */
     BLE_DIS,              /* Disable BLE HID sending              */
     BLE_EN,               /* Enable BLE HID sending               */
     DELBNDS,              /* Delete all bonding                   */
+    BATT_LV,              /* Display battery level in milli volts */
     ADV_ID0,              /* Start advertising to PeerID 0        */
     ADV_ID1,              /* Start advertising to PeerID 1        */
     ADV_ID2,              /* Start advertising to PeerID 2        */
@@ -37,11 +37,26 @@ enum custom_keycodes {
     DEL_ID4,              /* Delete bonding of PeerID 4           */
     ENT_SLP,              /* Deep sleep mode                      */
     LOWER,                /* Layer  keycode                       */
-    RAISE,                /* Layer  keycode                       */
+    RAISE                 /* Layer  keycode                       */
 };
 
 
 extern keymap_config_t keymap_config;
+
+enum {
+    KF_1 = 0, // 1, F1
+    KF_2, // ...
+    KF_3,
+    KF_4,
+    KF_5,
+    KF_6,
+    KF_7,
+    KF_8,
+    KF_9,
+    KF_10,
+    KF_11,
+    KF_12
+};
 
 enum {
   _QWERTY,
@@ -50,26 +65,7 @@ enum {
   _ADJUST
 };
 
-// Macros
-enum {
-  KF_1 = 0, // 1, F1
-  KF_2, // ...
-  KF_3,
-  KF_4,
-  KF_5,
-  KF_6,
-  KF_7,
-  KF_8,
-  KF_9,
-  KF_10,
-  KF_11,
-  KF_12
-};
-
 uint16_t kf_timers[12];
-
-#define LONGPRESS_DELAY 150
-#define LAYER_TOGGLE_DELAY 300
 
 // Layer related keycodes
 #define ADJUST MO(_ADJUST)
@@ -80,7 +76,7 @@ uint16_t kf_timers[12];
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( 
-    KC_ESC,   KC_1,    KC_2,     KC_3,     KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS, \
+    M(KF_12), M(KF_1), M(KF_2),  M(KF_3),  M(KF_4), M(KF_5),  M(KF_6), M(KF_7), M(KF_8), M(KF_9),  M(KF_10), M(KF_11),\
     KC_TAB,   KC_Q,    KC_W,     KC_E,     KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC, \
     KC_LGUI,  KC_A,    KC_S,     KC_D,     KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,    KC_X,     KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT, \
@@ -88,31 +84,32 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   M(KF_11),\
+    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN,  KC_UNDS, \
     KC_TILD, KC_AT,   KC_HASH, KC_DLR,  KC_CIRC, KC_AMPR,     _______, _______, _______, _______,  _______,  _______, \
-    KC_CAPS, KC_LABK, KC_RABK, KC_LCBR, KC_RCBR, KC_COLN,     _______, _______, _______, _______,  _______,  _______, \
-    KC_QUES, KC_UNDS, KC_DQT,  KC_LPRN, KC_RPRN, KC_PIPE,     _______, _______, _______, _______,  _______,  _______, \
+    KC_CAPS, KC_QUES, XXXXXXX, KC_LCBR, KC_RCBR, KC_COLN,     _______, _______, _______, _______,  _______,  _______, \
+    _______, KC_UNDS, KC_DQT,  KC_LPRN, KC_RPRN, KC_PIPE,     _______, _______, _______, _______,  _______,  _______, \
                       _______, _______, _______, _______,     _______, _______, _______, _______ \
   ),
 
   [_RAISE] = LAYOUT(
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_7,    KC_8,    KC_9,     XXXXXXX,  KC_EQL,  \
-    _______, _______, _______, _______, _______, _______,     KC_PSCR, KC_4,    KC_5,    KC_6,     _______,  KC_RBRC, \
-    _______, _______, _______, _______, _______, _______,     KC_INS,  KC_1,    KC_2,    KC_3,     _______,  _______, \
-    KC_LSFT, _______, _______, _______, _______, _______,     KC_DEL,  KC_0,    KC_DOT,  KC_LT,    KC_GT,    _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  KC_EQL,  \
+    _______, _______, _______, _______, _______, _______,     KC_INS,  KC_7,    KC_8,    KC_9,     _______,  KC_RBRC, \
+    _______, _______, _______, _______, _______, _______,     KC_DEL,  KC_4,    KC_5,    KC_6,     KC_LT,    KC_GT,   \
+    _______, _______, _______, _______, _______, _______,     KC_0,    KC_1,    KC_2,    KC_3,     KC_DOT,   _______, \
                       _______, _______, _______, _______,     _______, _______, _______, _______ \
   ),
 
   [_ADJUST] = LAYOUT (
     _______, ADV_ID0, ADV_ID1, ADV_ID2, ADV_ID3, ADV_ID4,     _______, _______, _______, _______,  _______,  _______, \
     _______, DEL_ID0, DEL_ID1, DEL_ID2, DEL_ID3, DEL_ID4,     _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,   KC_BSLS, \
-    _______, _______, _______, _______, _______, DELBNDS,     _______, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT,  _______, \
-    _______, _______, BLE_EN,  BLE_DIS, ENT_SLP, AD_WO_L,     _______, _______, _______, _______,  _______,  _______, \
+    _______, BLE_EN,  BLE_DIS, ENT_SLP, AD_WO_L, DELBNDS,     _______, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT,  _______, \
+    _______, _______, _______, _______, _______, BATT_LV,     _______, _______, _______, _______,  _______,  _______, \
                       _______, _______, _______, _______,     _______, _______, _______, _______ \
  )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  char str[16];
   switch (keycode) {
     case LOWER:
       if (record->event.pressed) {
@@ -134,7 +131,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-  }
+  } 
+  
   if (record->event.pressed) {
     switch (keycode) {
     case DELBNDS:
@@ -151,6 +149,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       set_ble_enabled(false);
       return false;
       break;
+    case BATT_LV:
+      sprintf(str, "%4dmV", get_vcc());
+      send_string(str);
+      return false;
     case ADV_ID0:
       restart_advertising_id(0);
       return false;
@@ -197,32 +199,35 @@ static void m_handle_kf (keyrecord_t *record, uint8_t id) {
   if (record->event.pressed) {
     kf_timers[code] = timer_read ();
   } else {
-//  uint8_t kc_base;
+    uint8_t kc_base;
     uint8_t long_press = (kf_timers[code] && timer_elapsed (kf_timers[code]) > TAPPING_TERM);
 
     kf_timers[code] = 0;
 
     switch(id){
-//      case KF_1 ... KF_10:
-//        if (long_press) {
-//          // Long press
-//          kc_base = KC_F1;
-//        } else {
-//         kc_base = KC_1;
-//      }
-//        code += kc_base;
-//        break;
+      case KF_1 ... KF_10:
+        if (long_press) {
+          // Long press
+          kc_base = KC_F1;
+        } else {
+          kc_base = KC_1;
+        }
+        code += kc_base;
+        break;
+      case KF_12:
+        code = long_press ? KC_F12 : KC_ESC;
+        break;
       case KF_11:
-        code = long_press ? KC_F11 : KC_F12;
-      break;
-//    case KF_12:
-//       code = long_press ? KC_F12 : KC_EQUAL;
-//      break;
+        code = long_press ? KC_F11 : KC_MINS;
+        break;
     }
     register_code (code);
     unregister_code (code);
   }
 }
+
+const uint16_t PROGMEM fn_actions[] = {
+};
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
@@ -233,4 +238,3 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
   }
   return MACRO_NONE;
 };
-
